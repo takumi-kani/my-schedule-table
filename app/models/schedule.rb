c7f1da56a814103a91f77b5d1e17775009e38968
takumi-kani <hxjtp796@gmail.com>
2021-09-14 17:23:22 +0900

    カレンダー表示

--- app/models/schedule.rb
+++ app/models/schedule.rb
@@ -1,2 +1,12 @@
 class Schedule < ApplicationRecord
+  with_options presence: true do
+    validates :title
+    validates :start_time
+    validates :end_time
+    validates :place
+    validates :info
+  end
+
+  belongs_to :user
+  has_many :comments
 end
--- config/routes.rb
+++ config/routes.rb
@@ -8,5 +8,5 @@ Rails.application.routes.draw do
   }
   resources :users, only: [:edit, :update]
 
-  resources :schedules, only: :index
+  resources :schedules
 end
